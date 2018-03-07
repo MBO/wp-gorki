4195209f7d057240a36f4d9c66e18a8fbd7b93e0
Mirosław Boruta <mirek@mirobor.pl>
2018-03-07 23:10:41 +0100

    Fix sidebar menu styles

--- sidebar.php
+++ sidebar.php
@@ -8,7 +8,7 @@
  */
 ?>
 
-<aside id="secondary" class="widget-area col-12 col-md-4 order-1">
+<aside id="secondary" class="widget-area col-12 col-md-4 col-lg-3 order-1">
 	<?php
 		wp_nav_menu( array(
 			'menu'            => 'aside',
@@ -17,7 +17,8 @@
 			'container_id'    => 'aside-menu',
 			// 'container_class' => 'collapse navbar-collapse',
 			'menu_id'         => false,
-			'menu_class'      => 'navbar-nav nav-column nav-pills',
+			// 'menu_class'      => 'navbar-nav nav-column nav-pills',
+			'menu_class'      => 'nav flex-column nav-pills',
 			'depth'           => 1,
 			'fallback_cb'     => 'bs4navwalker::fallback',
 			'walker'          => new bs4navwalker()
