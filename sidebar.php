2958d35422218111f25180563e0d89c69c5e19ad
Mirosław Boruta <mirek@mirobor.pl>
2018-03-08 00:20:30 +0100

    Experiment with sidebar menu

--- sidebar.php
+++ sidebar.php
@@ -8,24 +8,34 @@
  */
 ?>
 
-<aside id="secondary" class="widget-area col-12 col-md-4 col-lg-3 order-1">
-	<?php
-		wp_nav_menu( array(
-			'menu'            => 'aside',
-			'theme_location'  => 'menu-aside',
-			'container'       => 'div',
-			'container_id'    => 'aside-menu',
-			// 'container_class' => 'collapse navbar-collapse',
-			'menu_id'         => false,
-			// 'menu_class'      => 'navbar-nav nav-column nav-pills',
-			'menu_class'      => 'nav flex-column nav-pills',
-			'depth'           => 1,
-			'fallback_cb'     => 'bs4navwalker::fallback',
-			'walker'          => new bs4navwalker()
-		) );
-		
-		if (is_active_sidebar(' sidebar-1 ')) {
-			dynamic_sidebar( 'sidebar-1' );
-		}
-	?>
+<aside id="secondary" class="widget-area col-12 col-md-4 col-lg-3 order-1 mt-0 mb-0 d-flex">
+	<div class="card mt-0 mb-0">
+		<div class="card-body">
+			<h5 class="card-title">Menu</h5>
+			<div class="card-text">
+				<?php
+					wp_nav_menu( array(
+						'menu'            => 'aside',
+						'theme_location'  => 'menu-aside',
+						'container'       => 'div',
+						'container_id'    => 'aside-menu',
+						// 'container_class' => 'collapse navbar-collapse',
+						'menu_id'         => false,
+						// 'menu_class'      => 'navbar-nav nav-column nav-pills',
+						'menu_class'      => 'nav flex-column nav-pills',
+						'depth'           => 1,
+						'fallback_cb'     => 'bs4navwalker::fallback',
+						'walker'          => new bs4navwalker()
+					) );
+				?>
+			</div>
+		</div>
+		<?php if (is_active_sidebar(' sidebar-1 ')) { ?>
+		<div class="card-body">
+			<div class="card-text">
+				<?php dynamic_sidebar( 'sidebar-1' ); ?>
+			</div>
+		</div>
+		<?php } ?>
+	</div>
 </aside><!-- #secondary -->
