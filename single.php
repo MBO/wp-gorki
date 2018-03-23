994e256e49b583a242bd68998a872ed670552569
Mirosław Boruta <mirek@mirobor.pl>
2018-03-23 20:39:57 +0100

    Implemente single post template

--- single.php
+++ single.php
@@ -9,27 +9,21 @@
 
 get_header(); ?>
   <!-- single.php -->
-	<div id="primary" class="content-area container">
-		<main id="main" class="site-main">
+<div class="container mt-3 mb-3">
+  <div class="row">
+    <div class="col-12 col-lg-9 order-2">
+<?php
+while ( have_posts() ) : the_post();
 
-		<?php
-		while ( have_posts() ) : the_post();
+	get_template_part( 'template-parts/content', 'category' );
 
-			get_template_part( 'template-parts/content', get_post_type() );
+endwhile; // End of the loop.
+?>
+    </div>
 
-			the_post_navigation();
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+    <?php get_sidebar(); ?>
+  </div>
+</div>
 
 <?php
-get_sidebar();
 get_footer();
--- template-parts/content-category.php
+++ template-parts/content-category.php
@@ -12,27 +12,40 @@
 <article id="post-<?php the_ID(); ?>" <?php
  post_class("entry-content card border-left-0 border-right-0 round-0 mb-3 page-break");
 ?>>
-
-  <div class="card-body">
-  <div class="card-title">
-    <?php 
-    if ( is_singular() ) :
-			the_title( '<h1 class="entry-title display-3">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title display-3"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark" class="text-dark">', '</a></h2>' );
-    endif;
-    ?>
-  </div>
-  <div class="card-text">
   <?php
-    the_content();
-
-    wp_link_pages( array(
-      'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'gorki' ),
-      'after'  => '</div>',
-    ) );
+    if ( is_singular() ) :
   ?>
-  </div>
+    <div class="card-header p-0 rounded-0">
+      <nav aria-label="breadcrumb">
+        <ol class="breadcrumb m-0 rounded-0">
+          <li class="breadcrumb-item" aria-current="page"><?php the_category(', ') ?></li>
+          <li class="breadcrumb-item active"><?php the_title() ?></li>
+        </ol>
+      </nav>
+    </div>
+  <?php
+    endif;
+  ?>
+  <div class="card-body">
+    <div class="card-title">
+      <?php 
+      if ( is_singular() ) :
+        the_title( '<h1 class="entry-title display-3">', '</h1>' );
+      else :
+        the_title( '<h2 class="entry-title display-3"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark" class="text-dark">', '</a></h2>' );
+      endif;
+      ?>
+    </div>
+    <div class="card-text">
+    <?php
+      the_content();
+
+      wp_link_pages( array(
+        'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'gorki' ),
+        'after'  => '</div>',
+      ) );
+    ?>
+    </div>
   </div>
 </article><!-- #post-<?php the_ID(); ?> .entry-content -->
 
