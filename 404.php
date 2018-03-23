c4aa4575ed8371d5628f567d6b3ef8b5d1794798
Mirosław Boruta <mirek@mirobor.pl>
2018-03-23 22:26:55 +0100

    Add 404 page

--- 404.php
+++ 404.php
@@ -9,52 +9,22 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area container">
-		<main id="main" class="site-main">
-
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'gorki' ); ?></h1>
-				</header><!-- .page-header -->
-
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'gorki' ); ?></p>
-
-					<?php
-						get_search_form();
-
-						the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'gorki' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'gorki' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+<div class="container mt-3 mb-3">
+  <div class="row">
+    <div class="col-12 col-lg-9 order-2">
+			<div class="jumbotron text-right">
+				<h1 class="display-4">Nie znaleziono strony</h1>
+				<p class="lead text-right">Ten błąd oznacza, że strona pod podanym adresem nie istnieje.</p>
+				<hr>
+				<p class="text-right">Skorzystaj z menu po lewej lub kliknij:</p>
+				<p class="lead text-right">
+					<a class="bt btn-primary btn-lg" href="<?php echo get_home_url() ?>">Strona główna</a>
+				</p>
+			</div>
+		</div>
+		<?php get_sidebar(); ?>
+	</div>
+</div>
 
 <?php
 get_footer();
