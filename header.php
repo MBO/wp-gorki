cb47b549c722a51e6e2e1aec23ebef9ceb567699
Mirosław Boruta <boruta.miroslaw@gmail.com>
2020-09-19 13:02:05 +0200

    Update header to not put empty img tag

--- header.php
+++ header.php
@@ -54,6 +54,9 @@
 			</div>
 		</nav>
 
+		
+		<?php if ( has_header_image() ) { ?>
 		<img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
+		<?php } ?>
 	</header><!-- #masthead -->
 
